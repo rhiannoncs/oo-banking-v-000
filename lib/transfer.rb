c2a60d4e0015ae7e47e09b484960af00b5c87102
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-31 03:21:46 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   
   def execute_transaction
     unless status == "complete"
-      if !@sender.valid? || @amount > 
+      if !@sender.valid? || @amount > @sender.balance
         @status = "rejected"
         "Transaction rejected. Please check your account balance."
       else
