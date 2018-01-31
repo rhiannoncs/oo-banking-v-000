5b346a5f6d26cc9fe822b202054bc949567fd507
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-31 03:11:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,8 +8,12 @@ class BankAccount
     @status = "open"
   end
   
-  def deposit(money)
-    @balance += money
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def withdraw(amount)
+    @balance -= amount
   end
   
   def display_balance
